ec20cc006ee0a1f764506127c43cecac9f0d0d21
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-04-25 21:51:01 -0700

    Create Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,15 @@
+/*for(Initialization; condition; incr/decr)
+{
+ //statement
+}
+*/
+public class Forloop
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+    }    
+}
