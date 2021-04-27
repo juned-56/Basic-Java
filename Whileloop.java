8495649285656971ced2d2a2e47fc42eb91c6358
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-04-26 20:55:42 -0700

    Create Whileloop.java

---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+public class Whileloop
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        while(a < 10)
+        {
+            
+             System.out.println(a);
+            a++;
+        }
+    }    
+}
