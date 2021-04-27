662d729edfa3535bef346517701a09b409cde978
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-04-27 09:14:19 -0700

    Create Classobj.java

---
+++ Classobj.java
@@ -0,0 +1,19 @@
+//OOPs Concept in Java
+
+
+class Classobj
+{
+    int id;
+    String name;
+}
+class Test
+{
+public static void main(String[] args)
+    {
+        Classobj obj = new Classobj();
+        obj.id = 5;
+        obj.name = "Juned";
+        System.out.println("Id: "+obj.id);
+        System.out.println("Name: "+obj.name);
+    }
+}
