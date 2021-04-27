4b6c2972af5a7644c70f7f0c65e003b46d66b759
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-04-26 21:00:50 -0700

    Create Dowhileloop.java

---
+++ Dowhileloop.java
@@ -0,0 +1,21 @@
+/*
+do
+{
+  //Your Code
+}while(Condition);
+*/
+
+
+
+public class Dowhileloop
+{
+    public static void main(String[] args)
+    {
+        int n = 1;
+        do
+        {
+            System.out.println(n);
+            n++;
+        }while(n < 11);
+    }    
+}
