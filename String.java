0fdcc2fe6e0e83dada7ee961a7ff30c70f62357c
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-11 09:03:14 -0700

    Create String.java

---
+++ String.java
@@ -0,0 +1,13 @@
+public class Stringg 
+{
+    public static void main(String[] args)
+    {
+        String s1 = "Hello";    //creating string by java string literal  
+        char ch[] = {'J','A','V','A'};  
+        String s2 = new String(ch);      //converting char array to string  
+        String s3 = new String("Program");    ////creating java string by new keyword  
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
