39f1caf8e9154a8db4f2996626919b66c40f2bc4
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-20 08:38:19 -0700

    Create Stringconcatenation.java

---
+++ Stringconcatenation.java
@@ -0,0 +1,27 @@
+//####################  Concatenation by + Operator  ##########################
+
+
+public class Stringconcatenation
+{
+    public static void main(String args[])
+    {  
+         String s1 = "Juned"+" Khan";  
+         System.out.println(s1); 
+       }  
+ }
+
+
+
+//###################  Concatenation by concat() method  ##########################
+
+
+public class Stringconcatenation
+{
+    public static void main(String args[])
+    { 
+      String s1 = "Juned ";  
+      String s2 = "Khan";  
+      String s3 = s1.concat(s2);  
+      System.out.println(s3);
+    }
+}
