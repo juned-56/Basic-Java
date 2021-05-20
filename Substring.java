7b863251293f8fc71363dfb38e63e47983e08672
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-20 08:43:14 -0700

    Create Substring.java

---
+++ Substring.java
@@ -0,0 +1,13 @@
+//StartIndex: Inclusive
+//EndIndex: Exclusive
+
+
+public class Substring
+{
+    public static void main(String args[])
+    {  
+        String s="JunedKhan";  
+        System.out.println(s.substring(5)); 
+        System.out.println(s.substring(0,5)); 
+      }     
+}
