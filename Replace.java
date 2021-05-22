44373e8e960de8ece3571e0180f8a24749dd835a
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-21 20:15:13 -0700

    Create Replace.java

---
+++ Replace.java
@@ -0,0 +1,11 @@
+public class Replace
+{
+ 
+        public static void main(String args[])
+    {  
+        String s1="java is the most powerful progamming language";  
+        String replaceString=s1.replace('a','j');
+        System.out.println(replaceString);  
+    }
+}      
+
