3da79254cd19ed30f3fb49c96bd865f2f4153aaf
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-21 20:11:40 -0700

    Create Stringlength.java

---
+++ Stringlength.java
@@ -0,0 +1,10 @@
+public class Stringlength
+{
+    public static void main(String args[])
+    {  
+        String str1 = "Junedkhan";  
+        String str2 = "Javedkhan";  
+        System.out.println("string length is: "+str1.length());
+        System.out.println("string length is: "+str2.length());
+        }
+}
