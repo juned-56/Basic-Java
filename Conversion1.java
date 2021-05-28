3918c84a6039ac0169ccd378bfb0c6056529b256
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-27 20:28:25 -0700

    Create Conversion1.java

---
+++ Conversion1.java
@@ -0,0 +1,12 @@
+
+//############# String to Integer Conversion  ###############################
+
+public class Conversion1
+{
+    public static void main(String[] args)
+    {
+        String s = "60000";
+        int n = Integer.parseInt(s);
+        System.out.println(n);
+    }    
+}
