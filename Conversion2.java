c97d050a5820d6b7193074f4f52ed8c97987c15f
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-27 20:41:51 -0700

    Create Conversion2.java

---
+++ Conversion2.java
@@ -0,0 +1,13 @@
+
+//############################# Integer to String Coversion using valueOf() method  ##############################
+
+public class Conversion2
+{
+    public static void main(String[] args)
+    {
+        int n = 500;
+        String s = String.valueOf(n);
+        System.out.println(100+s);
+    }    
+}
+
