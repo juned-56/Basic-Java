d4a9d2726071547cffd80f752b4e4a8ae4c1b90d
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-30 08:53:02 -0700

    Create Conversion4.java

---
+++ Conversion4.java
@@ -0,0 +1,10 @@
+public class Conversion4
+{
+    public static void main(String[] args)
+    {
+        int REDIX = 16;
+        int a = 8;
+        char b = Character.forDigit(a,REDIX);
+        System.out.println(b);
+    }    
+}
