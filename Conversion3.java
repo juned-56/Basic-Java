bae16d4bb457cae9f2e10cf872c3794e86c9b1f6
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-30 08:46:44 -0700

    Update Conversion3.java

--- Conversion3.java
+++ Conversion3.java
@@ -1,4 +1,4 @@
-//#################### Char to Integer Conversion Using Character.getNumericValue() Method  #########################
+//#################### Char to Int Conversion Using Character.getNumericValue() Method  #########################
 
 public class Conversion3
 {
@@ -11,3 +11,15 @@ public class Conversion3
 }
 
 
+//#################### Char to Int Conversion Using String.valueOf() Method  #########################
+
+
+public class Conversion3
+{
+    public static void main(String[] args)
+    {
+        char a = '6';
+        int b = Integer.parseInt(String.valueOf(a));
+        System.out.println(b);
+    }
+}
