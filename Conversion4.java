88e0232c7a7e56158d3a792686272267f36e0808
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-30 08:55:36 -0700

    Update Conversion4.java

--- Conversion4.java
+++ Conversion4.java
@@ -1,8 +1,10 @@
+//####################### Char to Int Conversion Using Character.forDigit() Method  #############################
+
 public class Conversion4
 {
     public static void main(String[] args)
     {
-        int REDIX = 16;
+        int REDIX = 16;  //REDIX 10 for decimal and for hexa use redix 16 
         int a = 8;
         char b = Character.forDigit(a,REDIX);
         System.out.println(b);
