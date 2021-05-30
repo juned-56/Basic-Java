c304832d095ec5e6162e649a275268e898928e28
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-30 08:45:44 -0700

    Create Conversion3.java

---
+++ Conversion3.java
@@ -0,0 +1,13 @@
+//#################### Char to Integer Conversion Using Character.getNumericValue() Method  #########################
+
+public class Conversion3
+{
+    public static void main(String[] args)
+    {
+        char a = '6';
+        int b = Character.getNumericValue(a);
+        System.out.println(b);
+    }    
+}
+
+
